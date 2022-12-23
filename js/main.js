cbfc374a229285ad53648f2c21d6d290abb98bdc
stasfilippov <kron888999@gmail.com>
2022-12-23 14:50:32 +0300

    fix:icons svg sprite

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo-svg use");
 window.addEventListener('scroll', () => {
 	if (this.scrollY > 1) {
 		navbar.classList.add("navbar-light");
+		logo.href.baseVal = "icon/sprite.svg#logo-black";
 	} else {
 		navbar.classList.remove("navbar-light");
+		logo.href.baseVal = "icon/sprite.svg#logo-light";
 	}
 })
