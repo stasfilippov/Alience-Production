ac97aa4b5466dabfcbac9e5c4e5e100858f6684f
stasfilippov <kron888999@gmail.com>
2022-12-30 00:54:35 +0300

    add:responsive device<w980

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
+
 window.addEventListener('scroll', () => {
 	if (this.scrollY > 1) {
 		navbar.classList.add("navbar-light");
@@ -8,4 +11,8 @@ window.addEventListener('scroll', () => {
 		navbar.classList.remove("navbar-light");
 		logo.href.baseVal = "icon/sprite.svg#logo-light";
 	}
-})
+});
+mMenuToggle.addEventListener("click", (event) => {
+	event.preventDefault();
+	menu.classList.toggle("is-open");
+});
