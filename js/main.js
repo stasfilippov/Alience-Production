3fb27dd899ff7f8fa5534653476bc5623fa6e034
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-01-09 01:08:24 +0300

    fix:step-slider

--- js/main.js
+++ js/main.js
@@ -37,9 +37,32 @@ mMenuToggle.addEventListener("click", (event) => {
 	menu.classList.contains("is-open") ? closeMenu() : openMenu();
 });
 
-const swiper = new Swiper(".swiper", {
+const swiperSteps = new Swiper(".steps-slider", {
 	speed: 400,
-	autoHeight: false,
+	slidesPerView: 1,
+	navigation: {
+		nextEl: '.slider-button-next',
+		prevEl: '.slider-button-prev',
+	},
+	breakpoints: {
+		// when window width is >= 576px
+		545: {
+			slidesPerView: 2,
+		},
+		// when window width is >= 768px
+		787: {
+			slidesPerView: 3,
+		},
+		// when window width is >= 1024px
+		1024: {
+			slidesPerView: 4,
+		},
+	},
+});
+
+
+const swiper = new Swiper(".features-slider", {
+	speed: 400,
 	slidesPerView: 1,
 	navigation: {
 		nextEl: '.slider-button-next',
