ffb77ab5d4d26b083ae018ba158bf8e41a4dd302
stasfilippov <kron888999@gmail.com>
2023-01-23 22:30:48 +0300

    fix:modal-switcher

--- js/main.js
+++ js/main.js
@@ -88,8 +88,6 @@ const swiperSteps = new Swiper(".steps-slider", {
 	},
 });
 
-
-
 const swiperBlog = new Swiper(".blog-slider", {
 	speed: 400,
 	slidesPerView: 1,
@@ -108,4 +106,19 @@ const swiperBlog = new Swiper(".blog-slider", {
 			slidesPerView: 2,
 		},
 	},
+});
+
+const modal = document.querySelector(".modal");
+const modalToggle = document.querySelectorAll("[data-toggle=modal]");
+const modalClose = document.querySelector(".modal-close");
+console.log(modalToggle);
+modalToggle.forEach((element) => {
+	element.addEventListener("click", (event) => {
+		event.preventDefault();
+		modal.classList.add("is-open");
+	});
+});
+modalClose.addEventListener("click", (event) => {
+	event.preventDefault();
+	modal.classList.remove("is-open");
 });
