cf037a83c05c9b778fc8a214e1bb2bf96657af3a
stasfilippov <kron888999@gmail.com>
2023-02-04 01:06:39 +0300

    add: contracts-page

---
+++ autohim.php
@@ -0,0 +1,77 @@
+<?php
+$page_title = "Контрактное производство";
+$header_style = "header-inner-page";
+$Descr_Image = "contracts";
+$Descr_Title = "Контрактное производство автомобильной химии";
+$Descr_Text = "Прежде всего, начало повседневной работы по формированию позиции однозначно фиксирует необходимость приоритизации разума над эмоциями. Кстати, представители современных социальных резервов неоднозначны и будут описаны максимально подробно. </br> </br>
+Но социально-экономическое развитие напрямую зависит от поставленных обществом задач! Безусловно, постоянный количественный рост и сфера нашей активности предоставляет широкие возможности для системы обучения кадров, соответствующей насущным потребностям. В целом, конечно, выбранный нами инновационный путь обеспечивает широкому кругу (специалистов) участие в формировании своевременного выполнения сверхзадачи. А ещё элементы политического процесса представлены в исключительно положительном свете. Высокий уровень вовлечения представителей целевой аудитории является четким доказательством простого факта: высокотехнологичная концепция общественного уклада создаёт предпосылки для глубокомысленных рассуждений. В своём стремлении повысить качество жизни, они забывают, что дальнейшее развитие различных форм деятельности, а также свежий взгляд на привычные вещи - безусловно открывает новые горизонты для поставленных обществом задач.";
+include_once('./tamplate-parts/header-page.php');
+include_once("./tamplate-parts/page-header-descr-block.php"); ?>
+<section class="section make-autochim">
+    <div class="container">
+        <div class="make-autochim-content-wrapper">
+            <div class="make-autochim-content">
+                <div class="seporator"></div>
+                <h2 class="make-autochim-title">Изготовление автохимии</h2>
+                <p class="make-autochim-text">Но современная методология разработки позволяет оценить значение прогресса профессионального сообщества. Есть над чем задуматься: тщательные исследования конкурентов набирают популярность среди определенных слоев населения, а значит, должны быть ассоциативно распределены по отраслям. В своём стремлении повысить качество жизни, они забывают, что дальнейшее развитие различных форм деятельности, а также свежий взгляд на привычные вещи - безусловно открывает новые горизонты для поставленных обществом задач.</p>
+                <ul class="clients-list">
+                    <li class="clients-item">
+                        <svg height="30" width="30" class="clients-item-img">
+                            <use href="icon/sprite.svg#him"></use>
+                        </svg>
+                        Полное описание товаров
+                    </li>
+                    <li class="clients-item">
+                        <svg height="30" width="30" class="clients-item-img">
+                            <use href="icon/sprite.svg#car"></use>
+                        </svg>
+                        Актуальные цены
+                    </li>
+                    <li class="clients-item">
+                        <svg height="30" width="30" class="clients-item-img">
+                            <use href="icon/sprite.svg#him"></use>
+                        </svg>
+                        Полное описание товаров
+                    </li>
+                    <li class="clients-item">
+                        <svg height="30" width="30" class="clients-item-img">
+                            <use href="icon/sprite.svg#car"></use>
+                        </svg>
+                        Актуальные цены
+                    </li>
+                    <li class="clients-item">
+                        <svg height="30" width="30" class="clients-item-img">
+                            <use href="icon/sprite.svg#brush"></use>
+                        </svg>
+                        Лаки и краски
+                    </li>
+                    <li class="clients-item">
+                        <svg height="30" width="30" class="clients-item-img">
+                            <use href="icon/sprite.svg#him"></use>
+                        </svg>
+                        Химические производства
+                    </li>
+                    <li class="clients-item">
+                        <svg height="30" width="30" class="clients-item-img">
+                            <use href="icon/sprite.svg#brush"></use>
+                        </svg>
+                        Лаки и краски
+                    </li>
+                    <li class="clients-item">
+                        <svg height="30" width="30" class="clients-item-img">
+                            <use href="icon/sprite.svg#him"></use>
+                        </svg>
+                        Химические производства
+                    </li>
+                </ul>
+            </div>
+            <!-- /.make-autochim-content -->
+        </div>
+        <!-- /.make-autochim-content-wrappper -->
+    </div>
+    <!-- /.container -->
+    <img src="image/image-make-autochim.png" alt="make-autochim" class="make-autochim-image">
+</section>
+<!-- /.section make-autochim -->
+
+<?php include_once('./tamplate-parts/footer.php') ?>
